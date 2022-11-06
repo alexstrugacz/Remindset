7647391469819405649aeaa742907011d57ed422
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-05 23:37:49 -0500

    Initial Commit

---
+++ remindset/remindsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  remindsetApp.swift
+//  remindset
+//
+//  Created by Alex Strugacz on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct remindsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
