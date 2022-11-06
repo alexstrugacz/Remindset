e23556b41ee4c926dd78e3694331656b89941639
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-06 01:09:23 -0500

    Onboarding view done

---
+++ remindset/Screens/OnboardingView.swift
@@ -0,0 +1,127 @@
+//
+//  OnboaringView.swift
+//  remindset
+//
+//  Created by Alex Strugacz on 11/6/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    //MARK: - BODY
+    var body: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack(spacing: 20) {
+                //MARK: - HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                It's not how much we give but how much love we put into giving.
+                """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: HEADER
+                //MARK: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                
+                // MARK: - FOOTER
+                
+                ZStack {
+                    //PARTS OF MAJOR CUSTOM BUTTON
+                    
+                    //1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    
+                    
+                    //2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    //3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    //4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                    .foregroundColor(.white)
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .onTapGesture {
+                        isOnboardingViewActive = false
+                    }
+                        
+                        Spacer()
+                    }
+                    
+                    
+                    
+                } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding( )
+            } //: VSTACK ^ VSTACK CUSTOM
+        } //: ZSTACK
+    }
+}
+
+struct OnboaringView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
